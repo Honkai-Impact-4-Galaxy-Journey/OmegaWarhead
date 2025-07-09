4ab771dd505a4d45ce7d042c63df80d50d3d2b9d
Griseo-bh3rd <atmlz32@163.com>
2025-07-09 17:39:26 +0800

    feat: 一堆新功能

--- OmegaWarhead/PluginMain.cs
+++ OmegaWarhead/PluginMain.cs
@@ -5,12 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommandSystem;
+using CustomPlayerEffects;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Arguments.ServerEvents;
 using LabApi.Events.Arguments.WarheadEvents;
 using LabApi.Events.Handlers;
 using LabApi.Features.Wrappers;
 using LabApi.Loader.Features.Plugins;
 using MEC;
 using Mirror;
+using Respawning;
+using Respawning.Waves;
 using SCPSLAudioApi.AudioCore;
 using UnityEngine;
 
@@ -27,15 +32,44 @@ namespace OmegaWarhead
         public override Version Version => new Version("1.0.0");
 
         public override Version RequiredApiVersion => new Version("0.0.0.0");
+        public static CoroutineHandle coroutine;
 
         public override void Disable()
         {
-            WarheadEvents.Detonated -= (WarheadDetonatedEventArgs ev) => OmegaWarhead.ForceEnd = (Timing.RunCoroutine(OmegaWarhead.ForceEndRound()));
+            WarheadEvents.Detonated -= (WarheadDetonatedEventArgs ev) => { if (!OmegaWarhead.OmegaActivated) Timing.RunCoroutine(ForceEnd()); };
+            ServerEvents.RoundRestarted -= () => OmegaWarhead.StopOmega();
+            ServerEvents.RoundRestarted -= Music.OnRestartingRound;
+            PlayerEvents.InteractingScp330 -= OnPickingScp330;
+            ServerEvents.RoundRestarted -= () => Timing.KillCoroutines(coroutine);
+            WarheadEvents.Starting -= OnDeadmanActivating;
         }
 
         public override void Enable()
         {
-            WarheadEvents.Detonated += (WarheadDetonatedEventArgs ev) => OmegaWarhead.ForceEnd = (Timing.RunCoroutine(OmegaWarhead.ForceEndRound()));
+            WarheadEvents.Detonated += (WarheadDetonatedEventArgs ev) => { if (!OmegaWarhead.OmegaActivated) Timing.RunCoroutine(ForceEnd()); };
+            WarheadEvents.Starting += OnDeadmanActivating;
+            ServerEvents.RoundRestarted += () => OmegaWarhead.StopOmega();
+            ServerEvents.RoundRestarted += Music.OnRestartingRound;
+            ServerEvents.RoundRestarted += () => Timing.KillCoroutines(coroutine);
+            PlayerEvents.InteractingScp330 += OnPickingScp330;
+        }
+        public static void OnPickingScp330(PlayerInteractingScp330EventArgs ev)
+        {
+            System.Random random = new System.Random();
+            if (random.Next(1, 8) == 7) ev.CandyType = InventorySystem.Items.Usables.Scp330.CandyKindID.Pink;
+        }
+        public static void OnDeadmanActivating(WarheadStartingEventArgs ev)
+        {
+            if (ev.WarheadState.ScenarioType == WarheadScenarioType.DeadmanSwitch)
+            {
+                ev.IsAllowed = false;
+                if (!OmegaWarhead.OmegaActivated) OmegaWarhead.ForceEnd = (Timing.RunCoroutine(OmegaWarhead.OmegaMain()));
+            }
+        }
+        public static IEnumerator<float> ForceEnd()
+        {
+            yield return Timing.WaitForSeconds(180);
+            Round.End(force: true);
         }
     }
     public class FakeConnection : NetworkConnectionToClient
@@ -110,7 +144,6 @@ namespace OmegaWarhead
     {
         public static List<Player> Helikopter = new List<Player>();
         public static List<Player> Shelted = new List<Player>();
-        public static List<CoroutineHandle> coroutines = new List<CoroutineHandle>();
         public static bool OmegaActivated = false;
         public static CoroutineHandle ForceEnd;
         public static AudioPlayerBase playerBase;
@@ -118,9 +151,23 @@ namespace OmegaWarhead
         {
             if (OmegaActivated)
             {
-                foreach (CoroutineHandle coroutineHandle in coroutines) Timing.KillCoroutines(coroutineHandle);
+                Shelted.Clear();
+                Helikopter.Clear();
+                Timing.KillCoroutines(ForceEnd);
                 playerBase.Stoptrack(true);
                 Cassie.Message("pitch_0.9 Omega Warhead detonation stopped");
+                foreach (Door door in Door.List)
+                {
+                    if (door.DoorName == LabApi.Features.Enums.DoorName.LczCheckpointA ||
+                        door.DoorName == LabApi.Features.Enums.DoorName.LczCheckpointB ||
+                        door.DoorName == LabApi.Features.Enums.DoorName.Lcz914Gate ||
+                        door.DoorName == LabApi.Features.Enums.DoorName.HczCheckpoint ||
+                        door.DoorName == LabApi.Features.Enums.DoorName.EzGateA ||
+                        door.DoorName == LabApi.Features.Enums.DoorName.EzGateB)
+                    {
+                        door.IsLocked = false;
+                    }
+                }
                 foreach (Room room in Room.List)
                 {
                     foreach (var light in room.AllLightControllers)
@@ -128,7 +175,26 @@ namespace OmegaWarhead
                         light.OverrideLightsColor = Color.clear;
                     }
                 }
+                foreach (Elevator elevator in from e in Elevator.List
+                                              where e.Group == Interactables.Interobjects.ElevatorGroup.GateA || e.Group == Interactables.Interobjects.ElevatorGroup.GateB
+                                              select e)
+                {
+                    elevator.UnlockAllDoors();
+                }
                 OmegaActivated = false;
+            }
+        }
+        public static void StopEndRound()
+        {
+            Timing.KillCoroutines(OmegaWarhead.ForceEnd);
+            OmegaWarhead.playerBase.Stoptrack(true);
+            OmegaWarhead.OmegaActivated = false;
+            foreach (Room room in Room.List)
+            {
+                foreach (var light in room.AllLightControllers)
+                {
+                    light.OverrideLightsColor = Color.clear;
+                }
             }
         }
         public static IEnumerator<float> ForceEndRound()
@@ -145,7 +211,7 @@ namespace OmegaWarhead
             }
             Warhead.IsLocked = true;
             OmegaActivated = true;
-            playerBase = Music.PlayMusic("Omega", "Omega核弹警报", 40);
+            playerBase = Music.PlayMusic("Omega", "Omega核弹警报", 20);
             yield return Timing.WaitForSeconds(184);
             foreach (Player player in Player.List)
             {
@@ -159,48 +225,140 @@ namespace OmegaWarhead
                 }
             }
         }
-        [CommandHandler(typeof(GameConsoleCommandHandler))]
-        [CommandHandler(typeof(RemoteAdminCommandHandler))]
-        public class StartEndingRound : ICommand
+        public static IEnumerator<float> OmegaMain()
         {
-            public string Command => "startendround";
-
-            public string[] Aliases => Array.Empty<string>();
-
-            public string Description => "force end round in 180s";
-
-            public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+            if (OmegaActivated) { yield break; }
+            OmegaActivated = true;
+            Shelted.Clear();
+            Helikopter.Clear();
+            yield return Timing.WaitForSeconds(3);
+            Warhead.Stop();
+            foreach (Room room in Room.List)
             {
-                OmegaWarhead.ForceEnd = Timing.RunCoroutine(OmegaWarhead.ForceEndRound());
-                response = "Done!";
-                return true;
-            }
-        }
-        [CommandHandler(typeof(GameConsoleCommandHandler))]
-        [CommandHandler(typeof(RemoteAdminCommandHandler))]
-        public class StopEndingRound : ICommand
-        {
-            public string Command => "stopendround";
-
-            public string[] Aliases => Array.Empty<string>();
-
-            public string Description => "stop end round";
-
-            public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-            {
-                Timing.KillCoroutines(OmegaWarhead.ForceEnd);
-                OmegaWarhead.playerBase.Stoptrack(true);
-                OmegaWarhead.OmegaActivated = false;
-                foreach (Room room in Room.List)
+                foreach (var light in room.AllLightControllers)
                 {
-                    foreach (var light in room.AllLightControllers)
-                    {
-                        light.OverrideLightsColor = Color.clear;
-                    }
+                    light.OverrideLightsColor = Color.cyan;
                 }
-                response = "Done!";
-                return true;
             }
+            Warhead.IsLocked = true;
+            Server.SendBroadcast("<b><color=red>OMEGA核弹已启用.</color></b>\n请搭乘撤离直升机或前往地下掩体逃生.", 20);
+            playerBase = Music.PlayMusic("Omega", "Omega核弹警报", 20);
+            yield return Timing.WaitForSeconds(124);
+            foreach (Door door in Door.List)
+            {
+                if (door.DoorName == LabApi.Features.Enums.DoorName.LczCheckpointA ||
+                    door.DoorName == LabApi.Features.Enums.DoorName.LczCheckpointB ||
+                    door.DoorName == LabApi.Features.Enums.DoorName.Lcz914Gate ||
+                    door.DoorName == LabApi.Features.Enums.DoorName.HczCheckpoint ||
+                    door.DoorName == LabApi.Features.Enums.DoorName.EzGateA ||
+                    door.DoorName == LabApi.Features.Enums.DoorName.EzGateB)
+                {
+                    door.IsOpened = true;
+                    door.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Warhead, true);
+                }
+            }
+            yield return Timing.WaitForSeconds(38);
+            Server.SendBroadcast("撤离直升机将在12s后到达", 10, shouldClearPrevious: true);
+            WaveManager.TryGet<NtfSpawnWave>(out var wave);
+            WaveUpdateMessage.ServerSendUpdate(wave, UpdateMessageFlags.Trigger);
+            yield return Timing.WaitForSeconds(12);
+            Helikopter.AddRange(from p in Player.ReadyList
+                          where Vector3.Distance(p.Position, new Vector3(126, 295, -44)) <= 8
+                          select p);
+            Helikopter.ForEach(p => { p.EnableEffect<Flashed>(1, 15); p.EnableEffect<Ensnared>(1, 15); p.IsGodModeEnabled = true; });
+            yield return Timing.WaitForSeconds(10);
+            Shelted.AddRange(from p in Player.ReadyList
+                             where p.Room?.Name == MapGeneration.RoomName.EzEvacShelter
+                             select p);
+            Shelted.ForEach(p => { p.Position = (new Vector3(29, 293, -26)); p.EnableEffect<Flashed>(1, 5); });
+            foreach (Player player in Player.List)
+            {
+                if (!(Shelted.Contains(player) || Helikopter.Contains(player)) && player.IsAlive) player.Kill("在Omega核弹中蒸发");
+            }
+            foreach (Room room in Room.List)
+            {
+                foreach (var light in room.AllLightControllers)
+                {
+                    light.OverrideLightsColor = Color.blue;
+                }
+            }
+            foreach (Elevator elevator in from e in Elevator.List
+                                          where e.Group == Interactables.Interobjects.ElevatorGroup.GateA || e.Group == Interactables.Interobjects.ElevatorGroup.GateB
+                                          select e)
+            {
+                elevator.LockAllDoors();
+            }
+            Helikopter.ForEach(p => { p.IsGodModeEnabled = false; });
+            yield return Timing.WaitForSeconds(180);
+            Round.End(force:true);
+        }
+    }
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class StartEndingRound : ICommand
+    {
+        public string Command => "startendround";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "force end round in 180s";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!OmegaWarhead.OmegaActivated) OmegaWarhead.ForceEnd = Timing.RunCoroutine(OmegaWarhead.ForceEndRound());
+            response = "Done!";
+            return true;
+        }
+    }
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class StopEndingRound : ICommand
+    {
+        public string Command => "stopendround";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "stop end round";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            OmegaWarhead.StopOmega();
+            response = "Done!";
+            return true;
+        }
+    }
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class ActivateOmega : ICommand
+    {
+        public string Command => "activateomega";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "启动Omega核弹";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!OmegaWarhead.OmegaActivated) OmegaWarhead.ForceEnd = Timing.RunCoroutine(OmegaWarhead.OmegaMain());
+            response = "Done!";
+            return true;
+        }
+    }
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class StopOmega : ICommand
+    {
+        public string Command => "stopomega";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "关闭Omega核弹";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            OmegaWarhead.StopOmega();
+            response = "Done!";
+            return true;
         }
     }
 }
